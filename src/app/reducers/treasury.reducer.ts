73791d5ef0b2f64fe8e13bb135354b73bbed6198
vilkatis <garbalau@gmail.com>
2017-03-05 08:26:40 +0200

    trying to fix treasury

--- src/app/reducers/treasury.reducer.ts
+++ src/app/reducers/treasury.reducer.ts
@@ -4,27 +4,41 @@ import { ItemSlot } from '../models/gw2/account/item-slot.model';
 
 export interface State {
   ids: number[];
-  data: ItemSlot[];
+  data: { [id: number]: ItemSlot[] };
   loaded: boolean;
 }
 
 export const initialState: State = {
   ids: [],
-  data: [],
+  data: {},
   loaded: false
 };
 
 export function treasuryReducer(state = initialState, action: Action): State {
   switch (action.type) {
     case Gw2Actions.LOAD_TREASURY_SUCCESS: {
-      let mergedTreasury = [];
+      let mergedTreasury = {};
       let stateTreasury = state.data;
-      stateTreasury = stateTreasury.concat(action.payload);
-      stateTreasury.forEach((slot) => {
-        if (slot !== null) {
+      let addedData = action.payload;
+      addedData = addedData.reduce((previousValue, currentValue) => {
+        if (currentValue !== null) {
+          previousValue[currentValue.id] = currentValue;
         }
-      });
-      return Object.assign({}, state, { data: mergedTreasury, loaded: true });
+        return previousValue;
+      }, {});
+      for (let addedItem in addedData) {
+        if (addedData.hasOwnProperty(addedItem)) {
+          if (stateTreasury.hasOwnProperty(addedItem)) {
+            mergedTreasury[addedItem] = Object.assign({}, stateTreasury[addedItem]);
+            mergedTreasury[addedItem].count += addedData[addedItem].count;
+              console.log(mergedTreasury[addedItem].count, 'merge Item');
+              console.log(addedData[addedItem].count, 'added Item');
+          } else {
+            mergedTreasury[addedItem] = addedData[addedItem];
+          }
+        }
+      }
+      return Object.assign({}, state, { data: Object.assign({}, mergedTreasury), loaded: true });
     }
     default: {
       return state;
